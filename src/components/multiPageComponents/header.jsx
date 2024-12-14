a3bf01256c3332a06b7cc4b9aad7433d0e2fd76f
Will Rutland <wrutland07@gmail.com>
2024-12-14 21:15:06 +0000

    added header animations on hover

--- src/components/multiPageComponents/header.jsx
+++ src/components/multiPageComponents/header.jsx
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
 
+    function headerHovered(headerNo) {
+        document.querySelectorAll('.headerLine'+headerNo).forEach((headerLine) => {
+            headerLine.classList.add('hovered');
+        });
+    };
+
+    function headerUnhovered(headerNo) {
+        document.querySelectorAll('.headerLine'+headerNo).forEach((headerLine) => {
+            headerLine.classList.remove('hovered');
+        });
+    };
+
     function getHeaderCells() {
         let headerHTML = [];
         const allHeaders = [
@@ -14,15 +26,18 @@ export default function Header() {
             ['Support', 'support'],
             ['Sixth Form', 'sixthForm'],
         ];
-
-        allHeaders.forEach(([frontendName, backendName]) => {
+        allHeaders.forEach((header) => {
             headerHTML.push(
-                <td>
-                    <Link to={`/${backendName}`}>
-                        <h3 style={{marginTop: '32.5px', wordWrap:'revert'}}>
-                            {frontendName}
+                <td className="headerCell"
+                 onMouseOver={() => {headerHovered(allHeaders.indexOf(header))}}
+                onMouseLeave={() => {headerUnhovered(allHeaders.indexOf(header))}}>
+                    <div className={`headerLine headerLine${allHeaders.indexOf(header)}`}></div>
+                    <Link to={`/${header[1]}`}>
+                        <h3 style={{lineHeight: '20px', height: '20px', width: '100%', display: 'inline-block', verticalAlign: 'middle'}}>
+                            {header[0]}
                         </h3>
                     </Link>
+                    <div className={`headerLine headerLine${allHeaders.indexOf(header)}`}></div>
                 </td>
             );
         });
