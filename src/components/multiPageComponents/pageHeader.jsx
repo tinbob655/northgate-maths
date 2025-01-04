69c818ff04199a77c80be2eda627424af8f4043f
Will Rutland <wrutland07@gmail.com>
2025-01-04 15:39:05 +0000

    added northgate logo to page header

--- src/components/multiPageComponents/pageHeader.jsx
+++ src/components/multiPageComponents/pageHeader.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import DividerLine from './dividerLine';
+import northgateImage from '../../media/northgateImageNoBackground.png';
 
 export default function PageHeader({heading, subheading}) {
 
     return (
         <React.Fragment>
-            <h1 style={{marginLeft: '8%'}} className="alignLeft">
-                {heading}
-            </h1>
-            <p style={{marginLeft: '11%'}} className="alignLeft">
-                {subheading}
-            </p>
+            <table>
+                <thead>
+                    <tr>
+                        <td style={{width: '75%'}}>
+
+                            <h1 style={{marginLeft: '8%'}} className="alignLeft">
+                                {heading}
+                            </h1>
+                            <p style={{marginLeft: '11%'}} className="alignLeft">
+                                {subheading}
+                            </p>
+                        </td>
+                        <td>
+                            <img className="noVerticalSpacing" style={{height: '180px', float: 'right', marginRight: '10%', marginBottom: '40px'}} src={northgateImage} alt="The Northgate High School logo" />
+                        </td>
+                    </tr>
+                </thead>
+            </table>
             <DividerLine/>
         </React.Fragment>
     );
