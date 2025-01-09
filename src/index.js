c0a7bb87d5ce4a368e1816829a7ea0e24c041d67
Will Rutland <wrutland07@gmail.com>
2025-01-09 20:14:30 +0000

    made site scroll to top of page on page change

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import AllRoutes from './routes.jsx';
 import Header from './components/multiPageComponents/header.jsx';
 import Footer from './components/multiPageComponents/footer.jsx';
 import SitePasswordWrapper from './components/multiPageComponents/sitePasswordWrapper.jsx';
+import ScrollToTop from './components/multiPageComponents/scrollToTop.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <SitePasswordWrapper>
+        <ScrollToTop />
 
         <header>
           <Header/>
